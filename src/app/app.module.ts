ea8fd6c18f24d7fe4b9d78eb1a0b2cff6dc2d552
Alicia Yu <aliciayu.nz@gmail.com>
2018-04-11 21:49:36 +1200

    Angular Fundamentals
    Angular Fundamentals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.components';
+import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService,
+    AuthorsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/authors/authors.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorsService } from '../authors.service';
+
+@Component({
+  selector: 'authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+})
+export class AuthorsComponent implements OnInit {
+  title = "3 Authors";
+  authors;
+  constructor(service: AuthorsService) { 
+    this.authors = service.getAuthors();
+  }
+
+  ngOnInit() {
+  }
+
+}
